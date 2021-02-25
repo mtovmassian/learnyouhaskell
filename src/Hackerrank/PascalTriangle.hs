d1f7146d9a2cf47089aa81be11d2b4b13df061da
Martin Tovmassian <martin.tovmassian@protonmail.com>
2021-02-25 16:05:56 +0100

    chore: renaming in pascal triangle

--- src/Hackerrank/PascalTriangle.hs
+++ src/Hackerrank/PascalTriangle.hs
@@ -5,20 +5,21 @@ import Data.List
 factorial :: (Num a, Enum a) => a -> a
 factorial n = product [1..n]
 
-findBinomCoeff :: (RealFrac a, Enum a, Integral b) => a -> a -> b
+findBinomCoeff :: Double -> Double -> Integer
 findBinomCoeff n r = round $ nFactorial / (rFactorial * nrDiffFactorial)
   where
     nFactorial = factorial n
     rFactorial = factorial r
     nrDiffFactorial = factorial (n - r)
 
-findAllBinomCoeffs :: (RealFrac a, Enum a) => a -> [[Integer]]
+findAllBinomCoeffs :: Double -> [[Integer]]
 findAllBinomCoeffs kRows = [ map (findBinomCoeff rowIndex) [0..rowIndex] | rowIndex<-[0..(kRows - 1)]]
 
-drawPascalTriangle :: (RealFrac a, Enum a) => a -> [Char]
+drawPascalTriangle :: Double -> [Char]
 drawPascalTriangle kRows = pascalTriangleString
   where
-    pascalTriangleString = intercalate "\n" spacedRows
-    spacedRows = [unwords row | row<-stringifiedBinomCoeffs]
+    pascalTriangleString = intercalate "\n" spacedBinomCoeffs
+    spacedBinomCoeffs = [unwords row | row<-stringifiedBinomCoeffs]
     stringifiedBinomCoeffs = [ map show row | row<-binomCoeffs]
     binomCoeffs = findAllBinomCoeffs kRows
+ 

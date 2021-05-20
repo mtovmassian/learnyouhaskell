92291d83f0e1b9258a7d01854c04bac33bc3ce80
Martin Tovmassian <martin.tovmassian@protonmail.com>
2021-05-20 17:18:29 +0200

    imp polygon func to compute area

--- src/Hackerrank/PolygonPerimeter.hs
+++ src/Hackerrank/PolygonPerimeter.hs
@@ -10,6 +10,9 @@ getEdgeLength :: Edge -> Float
 getEdgeLength (Edge (Point x1 y1) (Point x2 y2)) = distancePoint1ToPoint2 where
   distancePoint1ToPoint2 = sqrt $ (x2 - x1) ** 2 + (y2 - y1) ** 2
 
+applyShoelaceFormula :: [Edge] -> Float
+applyShoelaceFormula edges_ = abs $ sum [(x1 * y2) - (y1 * x2) | (Edge (Point x1 y1) (Point x2 y2)) <- edges_] / 2
+
 toEdges :: [Point] -> [Edge]
 toEdges [] = []
 toEdges [_] = []
@@ -31,6 +34,9 @@ newPolygon points = Polygon { vertices = points, edges = toEdges connectedPoints
 
 perimeter :: Polygon -> Float
 perimeter Polygon {vertices = _, edges = edges_} = sum $ map getEdgeLength edges_
+
+area :: Polygon -> Float
+area Polygon {vertices = _, edges = edges_} = applyShoelaceFormula edges_
 
 main = do
   numberOfPoints <- getLine
